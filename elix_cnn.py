3cbf332ba1ac0376d662866170a3b3a843e1e860
Elixander <elixias@gmail.com>
2018-06-16 22:50:54 +0800

    Init CNN and added Conv Layer

--- elix_cnn.py
+++ elix_cnn.py
@@ -4,3 +4,7 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
+#init CNN
+classifier = Sequential()
+classifier.add(Convolution2D(32,3,3, input_shape=(64,64,3),activation='relu'))#32 filters size 3x3 with input size of 3 channels 64x64 !tensorflow format!
+#input_shape(3,64,64) #theano format
